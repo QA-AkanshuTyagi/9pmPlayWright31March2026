ac5257587658d66710229d950854ebdf8c8f253b
Nitendra <nitendragautam@paysecure.dev>
2026-04-27 12:34:59 +0530

    Updated again

--- Nitendra/05_String.js
+++ Nitendra/05_String.js
@@ -1,4 +1,16 @@
 let fName = "Nitendra";
 let lName = "Gautam";
 
-console.log(fName,lName)
+console.log(`My name is ${fName} ${lName}`)
+console.log(fName.concat(" "+lName))
+
+console.log(fName.length)
+console.log(fName.slice(2, 4))
+console.log(fName.toLowerCase())
+console.log(fName.charAt(2))
+if(fName.toLocaleLowerCase()===lName.toLocaleLowerCase()){
+console.log("strings matched")
+}
+else{
+    console.log("Strings don't match")
+}
