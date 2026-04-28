b278dfa559b38e5c6c8164ea0f4d67acf274bc98
Rahul Tandon <RahulT@sdgc.com>
2026-04-28 19:04:55 +0530

    rahul code added

---
+++ Rahul/03DataType.js
@@ -0,0 +1,15 @@
+// Arraray of array is called as multidimensional array
+//                      0 [0,1,2]                  1 [0,1,2,3]       2[0,1,2]             
+let studentDetails=[["rahul",18,"fail"],["neha",12,"pass",true],["noor",15,"pass"]]
+console.log(studentDetails[0][0]);
+
+
+let studentsDetails={
+
+student1:[["akhil",21,"hello"],["ranaji",23,"hi",false],["rani",51,"fail"]],
+student2:["neha",12,"pass",true],
+student3:["noor",15,"pass"]
+
+}
+
+console.log(studentsDetails.student1[0][2]);
---
+++ Rahul/Operators.js
@@ -0,0 +1,51 @@
+///Operators 
+
+//types: arithmatic operator, relational operator, assigment operator.
+
+//arithmatic : + - % / ,* ,**
+
+let num1=10;
+let num2=6;
+console.log(num1+num2); //addition
+console.log(num1-num2);//subtraction
+console.log(num1*num2);//multiplication
+console.log(num1/num2);//quotient
+console.log(num1%num2);//remainder
+console.log(10**6);//square
+
+//relational operator: > < >= <= == != === !==
+
+var a="2010"
+let b=20;
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);//a>b or a==b
+console.log(a<=b);  //  a<b or a==b
+
+console.log(a==b);//value is same or not
+console.log(a!=b);//value is not same or not
+console.log(a===b);//value and datatype is same or not
+console.log(a!==b);//value and datatype is not same or not
+
+//assignment operator: =, +=, -=, *=, /=, %=, **=
+
+//assigning of the value
+
+let x=10;
+x+=5
+// x+=5; //x=x+5
+console.log(x);
+
+x-=5
+console.log(x);
+x*=5;
+console.log(x);
+
+x/=5;
+console.log(x);
+
+x%=5;
+console.log(x);
+x=10;
+x**=5;
+console.log(x);
