8dba5af414da2a5963ac236eb9c227f6f9dbbfde
QA-Neha Noor <noor16ansari@gmail.com>
2026-04-28 21:57:27 +0530

    28April

---
+++ Neha Noor/ForLoop.js
@@ -0,0 +1,9 @@
+let sentence="neha is studying playwright";
+
+for(let i=1;i<=5;i++){
+    console.log("neha");
+}
+
+for(let i=5;i>=1;i--){
+    console.log(i);
+}
---
+++ Neha Noor/StringFunctions.js
@@ -0,0 +1,24 @@
+let name="  hello"
+console.log(name);
+console.log(name.includes())
+console.log(name.trim());
+
+let name1="neha is the trainer of testing"
+console.log(name1.startsWith("akanshu"));
+console.log("################")
+console.log(name1.endsWith("testing"));
+console.log("#######")
+console.log(name1.slice(0,7));
+
+console.log(name1.substring(0,7));
+
+console.log(name1.replace("neha","noor"));
+console.log(name1.replaceAll(" ","$"));
+
+console.log("##########")
+
+console.log(name1.split(" ")); //convert string to array
+
+console.log(name1.charAt(20));
+
+
