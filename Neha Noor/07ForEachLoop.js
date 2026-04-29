24a895503746e244eb00a26e114f30807e6b0a4e
QA-Neha Noor <noor16ansari@gmail.com>
2026-04-29 22:03:51 +0530

    29 April

---
+++ Neha Noor/07ForEachLoop.js
@@ -0,0 +1,15 @@
+let name="neha";
+
+let arr=[1,2,3,4,5];
+//console.log(arr[1]);
+
+arr.forEach(function(array){
+
+    console.log(array);
+})
+
+for(let number of arr){
+    console.log(number);
+
+}
+
