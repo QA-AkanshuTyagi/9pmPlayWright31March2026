97bed01f41e44469fdc0df4b7117a1d6d6bea5bf
Rahul Tandon <RahulT@sdgc.com>
2026-05-01 13:58:35 +0530

    rahul new code

---
+++ Rahul/ForEachLoop.js
@@ -0,0 +1,15 @@
+
+//forEach loop is used to iterate over an array
+let arr=[1,2,3,4,5];
+
+//printing elements of array using forEach method
+arr.forEach(function(numb){
+    console.log(numb);
+})
+
+//using for each loop to print the elements in an array
+
+for (let number of arr){
+    console.log(number);
+}
+
