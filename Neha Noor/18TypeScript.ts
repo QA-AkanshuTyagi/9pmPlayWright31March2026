43199ee3f9041ef6be080bef6fa36b528349df3a
QA-Neha Noor <noor16ansari@gmail.com>
2026-06-06 10:50:07 +0530

    changes done

---
+++ Neha Noor/18TypeScript.ts
@@ -0,0 +1,14 @@
+let loginPage=(firstName:string,LastName:string):string=>{
+    firstName="neha"
+    console.log("firstName"+"==>"+firstName+LastName)
+    return firstName+LastName
+}
+
+loginPage("neha","noor")
+loginPage("neha","sharma")
+
+
+
+
+
+
