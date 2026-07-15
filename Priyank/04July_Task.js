22587802b84441e0c38152c83bdf530adb11bb21
QA-Priyank <priyanksrivastava40@gmail.com>
2026-07-15 13:06:30 +0530

    New 15

--- Priyank/04July_Task.js
+++ Priyank/04July_Task.js
@@ -32,7 +32,7 @@ console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
 
 //3. Keyword in JS
 
-//4. Prime number from 100 to 1 in reverse order
+//4. Prime number from 100 to 1 in reverse order --------- not clear
 
 for (let i=100;i>=1;i--){
 
@@ -54,3 +54,23 @@ for (let i = 100; i >= 2; i--) {
         console.log(i);
     }
 }
+
+console.log('############################################################')
+
+// 5.write a program to print to reverse the string. HELLO TEST OF Playwright  
+// O/P Playwright fo tset hello
+
+let sentence = 'HELLO TEST OF Playwright'
+let sen1 = (sentence.split(" "))
+rev = " "
+
+for(i=sen1.length-1;i>=0;i--){
+
+    console.log(i)
+}
+
+
+
+//6.what are the different ways to find an element.
+// xpath, CSS and Get by method
+
