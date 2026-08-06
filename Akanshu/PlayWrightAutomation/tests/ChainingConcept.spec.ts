73013d881947be54112ac3b001168e633515961e
QA-AkanshuTyagi <tyagiakanshu2@gmail.com>
2026-08-06 21:53:21 +0530

    moving latest code

--- Akanshu/PlayWrightAutomation/tests/ChainingConcept.spec.ts
+++ Akanshu/PlayWrightAutomation/tests/ChainingConcept.spec.ts
@@ -10,3 +10,13 @@ console.log(tablevalues);
 
 
 });
+
+test('TableHandling1', async ({  page }) => {
+ 
+    await page.goto('https://testautomationpractice.blogspot.com/p/playwrightpractice.html');
+ 
+let tablevalues=await page.getByRole('table').filter({hasText: 'Learn Selenium'}).getByRole('row').filter({hasText:'Learn Selenium'}).getByRole("cell").nth(3).textContent();
+ 
+console.log(tablevalues);
+ 
+});
