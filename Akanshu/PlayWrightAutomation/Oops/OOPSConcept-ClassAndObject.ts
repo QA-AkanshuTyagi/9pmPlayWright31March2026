186c1608cae16c1ff70fd9d842dcdf4e780d4916
QA-AkanshuTyagi <tyagiakanshu2@gmail.com>
2026-09-01 23:03:22 +0530

    moving my latest that includes accessspecifier and modifier

---
+++ Akanshu/PlayWrightAutomation/Oops/OOPSConcept-ClassAndObject.ts
@@ -0,0 +1,31 @@
+ class LoginPage {
+    private name="akash" 
+      protected username="akanshu";
+
+public  login(){
+
+console.log("Login method is called");
+
+}
+
+}
+
+
+ //making an object
+
+class Homepage {
+     
+  public  callig(){
+
+    
+//classname.methodname
+
+
+  }   
+
+
+}
+const home= new Homepage();
+
+
+home.callig()
