4c02556ae38013c2f99a31662414337608abbe95
QA-Priyank <priyanksrivastava40@gmail.com>
2026-09-15 23:32:10 +0530

    New one

---
+++ Priyank/Revision/if_else.js
@@ -0,0 +1,35 @@
+/*
+if else - 
+if the statement is true - if code will execute
+otherwise else will work
+
+*/
+
+
+const age = 17
+
+if(age>=18){
+    console.log('eligible for Vote') 
+
+}else{
+    console.log('Not eligible')
+
+}
+
+
+const age1 = 20
+
+if(age1==18){
+    console.log('eligible for Vote') 
+
+}else if(age1>18) {
+
+    console.log('eligible for Vote as age> 18') 
+
+}
+
+else{
+    console.log('Not eligible')
+
+}
+
---
+++ Priyank/Revision/new.js
@@ -0,0 +1,51 @@
+// hoisting
+// console.log(abc)
+
+// const abc = 'Priyank'
+
+
+// +, - , *, /, %, **
+
+//console.log(!true)
+
+// ternary op
+// ? :
+
+// same as if else concept
+
+// const age = 17
+
+// if(age>=18){
+//     console.log('eligible for Vote') 
+
+// }else{
+//     console.log('Not eligible')
+
+// }
+
+// condtion ? value1 : value2
+
+// const age = 27
+
+// let abcds = age>=18 ? 'Vote' : 'Not Ele'
+
+// console.log(abcds)
+
+
+
+//var 1
+
+// outcome/condition - multiple cases but ek condition
+
+
+let day = 2
+
+switch (day) {
+    case 1: 
+    console.log("Monday")
+    break
+
+
+
+}
+
